e2b8e1c5f74b173803e68bfcc27fd749e14e7960
Gunsko020 <guner70@outlook.com>
2024-03-06 16:03:46 +0100

    eerste state gemaakt van info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import './App.css';
+import axios from "axios";
+import {useState} from "react";
+import worldmap from "./assets/world_map.png"
 
 function App() {
 
+    const [information,setInformation] = useState([])
+
+    async function fetchCountries() {
+        try{
+            const response = await axios.get('https://restcountries.com/v3.1/all')
+            // naam van het eerste land :
+            // console.log(response.data[0].name.common)
+            setInformation(response.data)
+        }catch (e) {
+            console.error(e)
+        }
+
+    }
+
+
     return (
+
         <>
-            Maak je applicatie hier!
+           <header><img src={worldmap} alt="worldmap" className="world-map"/></header>
+            <section>
+
+            <h1>World Regions</h1>
+            <button
+                type="button"
+                onClick={fetchCountries}
+            >
+                Haal alle landen op
+            </button>
+            <ul>
+
+            </ul>
+            </section>
         </>
+
     )
 }
 
